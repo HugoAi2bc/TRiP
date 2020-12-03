360019fdc61f9383494ba017c2c2e5521a35f069
HugoAi2bc <hugo.arbes@i2bc.paris-saclay.fr>
2020-12-03 06:20:01 +0100

    test

---
+++ TRiP_test.sh
@@ -0,0 +1,37 @@
+################################################################################
+#########################           TRiP           ################e#############
+################################################################################
+
+################################################################################
+### Tool for Ribosome Profiling
+### Pauline FRANCOIS ; Hugo ARBES
+### December 1st, 2020
+################################################################################
+
+######
+# Run this script with an interactive shell ()-i) :
+# bash -i TRiP.sh
+######
+
+
+# Need installation de locate : $ sudo apt install locate
+
+
+# conda --version;
+# source /home/hugo.arbes/miniconda3/etc/profile.d/conda.sh
+# MYSHELL=`basename $SHELL`;
+# conda init -v $MYSHELL;
+
+# conda activate all_TRiP;
+
+# conda list;
+snakemake -s /TRiP/Snakefile_test -j --dag -np |  dot -Tsvg > /data/dag_last-run.svg;
+snakemake -s /TRiP/Snakefile_test -j --dag -np --forceall |  dot -Tsvg > /data/dag_all.svg;
+snakemake -s /TRiP/Snakefile_test -j;
+
+# conda deactivate;
+
+
+# conda activate analysis
+# snakemake -s snakefile_analysis -j-np
+# conda deactivate
